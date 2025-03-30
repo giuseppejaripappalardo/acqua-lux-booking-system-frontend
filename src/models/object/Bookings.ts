4c686e8bb52cfe0ca39b738452f58229f8a38486
Giuseppe Jari Pappalardo <giuseppejaripappalardo@gmail.com>
2025-03-30 21:14:53 +0200

    Aggiornata interfaccia che causava problemi per la compilazione

--- src/models/object/Bookings.ts
+++ src/models/object/Bookings.ts
@@ -1,6 +1,8 @@
-import {BookingFlowState} from "../../pages/BookingFlowPage/BookingFlowPage.tsx";
-
 export type BookingFlowSteps = "search" | "payment" | "confirm";
 export type BookingPaymentMethods = "BANK_TRANSFER" | "CASH" | "CREDIT_CARD";
 
-export type BookingSearchFields = Omit<BookingFlowState, "selectedBoat" | "step" | "paymentMethod">
+export interface BookingSearchFields {
+    startDate: string;
+    endDate: string;
+    seats: number;
+}
